a88057afa2a5eeb0158081975abfce1cd31612f8
August (Chris) <augu@voided.pw>
2018-12-30 20:48:54 -0700

    fix: manifest version

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,8 @@
 declare module '@maika.xyz/eris-utils' {
     export const version: string;
     export class Collection<K, V> extends Map<K, V> {
+        public keyArray: K[];
+        public valArray: V[];
         public filter(fn: (i: V) => boolean): V[];
         public map(fn: (i: V) => any): V[];
     }
